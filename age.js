9274483c79fa10c15f7e34c962a96132a2f5853d
ATarsa <smb909@gmail.com>
2019-04-26 21:13:09 +0100

    Refactor age.js. Add interacitivy to change between stack and percent age stack

--- age.js
+++ age.js
@@ -32,118 +32,101 @@
   svgAgeStack.append("g")
     .attr("class", "y axis")
   
-  
 
   // create colours array
-  const coloursAgeStack = ["#ccebc5", "#b3cde3", "#fbb4ae", '#e41a1c','#377eb8','#4daf4a','#C7EFCF','#FE5F55','#EEF5DB', "yellow"];
+  const coloursAgeStack = ['#fbb4ae','#b3cde3','#ccebc5','#decbe4','#fed9a6','#ffffcc','#e5d8bd','#fddaec','#f2f2f2'];
   
-  // Create div for tooltip
+  // //Create div for tooltip
   // let div = d3.select("body")
   //   .append("div")
   //   .attr("class", "tooltip")
   //   .style("opacity", 0);
+    
+    function showAgeStackChart(){
 
-
-
-
-
-
-
-function showAgeStackChart(){
-  // Set x, y
-  let x = d3.scaleBand()
+      // Get the data
+      d3.csv("age_groups.csv", type, function (error, data) {
+      
+            // Set x, y
+      let x = d3.scaleBand()
       .range([0, widthAgeStack])
       .padding(0.01);
 
-  let y = d3.scaleLinear()
-      .range([heightAgeStack, 0]);
+      let y = d3.scaleLinear()
+        .range([heightAgeStack, 0]);
 
-  let x_axis =d3.axisBottom(x).tickSizeOuter(0);
-  let y_axis = d3.axisLeft(y);
-      
-  // Get the data
-  d3.csv("age_groups.csv", type, function (error, data) {
+      let x_axis =d3.axisBottom(x).tickSizeOuter(0);
+      let y_axis = d3.axisLeft(y);
+      // list of subgroups (header of csv files)
+      let subgroups = data.columns.slice(1);
 
-    if (error) throw error;
+      // list of years -> x axis
+      let years = d3.map(data, function (d) {
+        return d.Year
+      }).keys();
 
-    // create transition object
-    let t = d3.transition()
-        .duration(1500);
+
+      // Set x domain  and colours
+      x.domain(years)
         
-    // list of subgroups (header of csv files)
-    let subgroups = data.columns.slice(1);
+      let colour = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(coloursAgeStack); // TODO: change colors
+          // create transition object
+      let t = d3.transition()
+        .duration(1500);
 
-    // list of years -> x axis
-    let years = d3.map(data, function (d) {
-      return d.Year
-    }).keys();
-
-
-    // Set x, y and colours
-    x.domain(years)
-      
-    y.domain([0, d3.max(data, function (d) {
+      y.domain([0, d3.max(data, function (d) {
         return d.total;
       })]).nice();
 
     
-    let colour = d3.scaleOrdinal()
-      .domain(subgroups)
-      .range(["#ccebc5", "#b3cde3", "#fbb4ae", '#e41a1c','#377eb8','#4daf4a','#C7EFCF','#FE5F55','#EEF5DB', "yellow"]); // TODO: change colors
-    
-    
      // stack the data
      let stackedData = d3.stack()
      .keys(subgroups)
      (data)
 
-  // If percent stack chart present remove it 
-   svgAgeStack.selectAll(".percent")
-       .remove()
+     // If percent percent chart present remove it 
+      svgAgeStack.selectAll(".percent")
+          .remove()
 
        // show the bars
-   let bars = svgAgeStack
-     .selectAll(".stack")
-     .data(stackedData)
-     .attr("fill", function (d) {
-      return colour(d.key);
-    })
+      let bars = svgAgeStack
+        .selectAll(".stack")
+        .data(stackedData)
+        .attr("fill", function (d) {
+          return colour(d.key);
+        })
     
+      bars.enter()
+          .remove()
 
-
-    bars.enter()
-        .remove()
-
-    
-    // enter new
-    let newBars = bars
-      .enter()
-      .append("g")
-      .attr("class", "stack")
-        .attr('height', 0)
-        .attr('y', heightAgeStack)
-       .attr("fill", function (d) {
-       return colour(d.key);
-     })
+      // enter new
+      let newBars = bars
+        .enter()
+        .append("g")
+        .attr("class", "stack")
+          .attr('height', 0)
+          .attr('y', heightAgeStack)
+        .attr("fill", function (d) {
+        return colour(d.key);
+      })
      
-     .selectAll("g")
-     // enter a second time -> loop subgroups to add all rectangles
-     .data(function (d) {
-       return d;
-     })
-     .enter()
-     .append("rect")
+      .selectAll("g")
+      // enter a second time -> loop subgroups to add all rectangles
+      .data(function (d) {
+        return d;
+      })
+      .enter()
+      .append("rect")
 
-     newBars.merge(bars)
-     .transition(t)
-     .attr("x", function (d) {return x(d.data.Year); })
-     .attr("y", function (d) { return y(d[1]);})
-     .attr("height", function (d) { return y(d[0]) - y(d[1]); })
-     .attr("width", x.bandwidth())
+      newBars.merge(bars)
+        .transition(t)
+        .attr("x", function (d) {return x(d.data.Year); })
+        .attr("y", function (d) { return y(d[1]);})
+        .attr("height", function (d) { return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth())
 
-
-        
-      
       newBars.on("mouseover", function (d) {
         div.transition()
           .duration(200)
@@ -167,7 +150,6 @@ function showAgeStackChart(){
       });
 
 
-
      // Call the  axis
       svgAgeStack.select('.x.axis')
             .transition(t)
@@ -179,160 +161,168 @@ function showAgeStackChart(){
 
 
       addLegendAgeStack(data)
-
-  });
-
-
-}
-function percentStack(){
-  // Set x, y
-  let x = d3.scaleBand()
-      .range([0, widthAgeStack])
-      .padding(0.01);
-
-  let y = d3.scaleLinear()
-      .range([heightAgeStack, 0]);
-      
-  let x_axis =d3.axisBottom(x).tickSizeOuter(0);
-  let y_axis = d3.axisLeft(y);
-  
-  // Get the data
-  d3.csv("age_groups.csv", type, function (error, data) {
-
-    if (error) throw error;
-
-    // create transition object
-    let t = d3.transition()
-        .duration(1500);
-        
-    // list of subgroups (header of csv files)
-    let subgroups = data.columns.slice(1);
-
-    // list of years -> x axis
-    let years = d3.map(data, function (d) {
-      return d.Year
-    }).keys();
-
-
-    // Set x, y and colours
-    x.domain(years)
-      
-    y.domain([0,100]);
-
-    
-    let colour = d3.scaleOrdinal()
-      .domain(subgroups)
-      .range(["#ccebc5", "#b3cde3", "#fbb4ae", '#e41a1c','#377eb8','#4daf4a','#C7EFCF','#FE5F55','#EEF5DB', "yellow"]); // TODO: change colors
-    
-    // Convert numbers to % of whole
-    dataPercent = [];
-
-    data.forEach(function(d){
-      // compute total
-      total = 0;
-      for (i in subgroups){
-        columnName = subgroups[i];
-        total += +d[columnName];
-      }
-      // compute percent
-      for (i in subgroups){
-        columnName = subgroups[i];
-        d[columnName] = d[columnName] / total * 100;
-      }
-    });
-    console.log(data)
-     // stack the data
-     let stackedData = d3.stack()
-     .keys(subgroups)
-     (data)
-
-  // If stack chart present remove it 
-   svgAgeStack.selectAll(".stack")
-       .remove()
-
-       // show the bars
-   let bars = svgAgeStack
-     .selectAll(".percent")
-     .data(stackedData)
-     .attr("fill", function (d) {
-      return colour(d.key);
     })
-    
+  };
+
+    function showAgePercentStack(data){
+
+      // Get the data
+      d3.csv("age_groups.csv", type, function (error, data) {
+
+        if (error) throw error;
+     
+      
+              // Set x, y
+        let x = d3.scaleBand()
+        .range([0, widthAgeStack])
+        .padding(0.01);
+
+      let y = d3.scaleLinear()
+        .range([heightAgeStack, 0]);
+
+      let x_axis =d3.axisBottom(x).tickSizeOuter(0);
+      let y_axis = d3.axisLeft(y);
+        // list of subgroups (header of csv files)
+        let subgroups = data.columns.slice(1);
+
+        // list of years -> x axis
+        let years = d3.map(data, function (d) {
+          return d.Year
+        }).keys();
 
 
-    bars.enter()
+        // Set x domain  and colours
+        x.domain(years)
+          
+        let colour = d3.scaleOrdinal()
+          .domain(subgroups)
+          .range(coloursAgeStack); // TODO: change colors
+      
+      // create transition object
+      let t = d3.transition()
+      .duration(1000);
+
+      y.domain([0,100]);
+
+      ;
+      data.forEach(function(d){
+        // compute total
+        total = 0;
+        for (i in subgroups){
+          columnName = subgroups[i];
+          total += +d[columnName];
+        }
+        // compute percent
+        for (i in subgroups){
+          columnName = subgroups[i];
+          d[columnName] = d[columnName] / total * 100;
+        }
+      });
+      
+      // stack the data
+      let stackedData = d3.stack()
+      .keys(subgroups)
+      (data)
+
+    // If stack chart present remove it 
+    svgAgeStack.selectAll(".stack")
         .remove()
 
-    
-    // enter new
-    let newBars = bars
+        // show the bars
+    let bars = svgAgeStack
+      .selectAll(".percent")
+      .data(stackedData)
+      .attr("fill", function (d) {
+        return colour(d.key);
+      })
+      
+      bars.enter()
+          .remove()
+
+      // enter new
+      let newBars = bars
+        .enter()
+        .append("g")
+        .attr("class", "percent")
+          .attr('height', 0)
+          .attr('y', heightAgeStack)
+        .attr("fill", function (d) {
+        return colour(d.key);
+      })
+      
+      .selectAll("g")
+      // enter a second time -> loop subgroups to add all rectangles
+      .data(function (d) {
+        return d;
+      })
       .enter()
-      .append("g")
-      .attr("class", "percent")
-        .attr('height', 0)
-        .attr('y', heightAgeStack)
-       .attr("fill", function (d) {
-       return colour(d.key);
-     })
-     
-     .selectAll("g")
-     // enter a second time -> loop subgroups to add all rectangles
-     .data(function (d) {
-       return d;
-     })
-     .enter()
-     .append("rect")
+      .append("rect")
 
-     newBars.merge(bars)
-     .transition(t)
-     .attr("x", function (d) {return x(d.data.Year); })
-     .attr("y", function (d) { return y(d[1]);})
-     .attr("height", function (d) { return y(d[0]) - y(d[1]); })
-     .attr("width", x.bandwidth())
+      newBars.merge(bars)
+      .transition(t)
+      .attr("x", function (d) {return x(d.data.Year); })
+      .attr("y", function (d) { 
+        let total = d3.sum(d3.values(d.data));
+        return y(d[1]) ;})
+      .attr("height", function (d) { 
+        let total = d3.sum(d3.values(d.data));
+       
+        return y(d[0]) - y(d[1]) ; })
+      .attr("width", x.bandwidth())
 
+        newBars.on("mouseover", function (d) {
+          div.transition()
+            .duration(200)
+            .style("opacity", .9);
+
+          // subgroup Name and Value with reference to https://www.d3-graph-gallery.com/graph/barplot_stacked_hover.html 
+          let subgroupName = d3.select(this.parentNode).datum().key;
+          let subgroupValue = d.data[subgroupName];
+          div.html(`${subgroupName}: <br>${subgroupValue}`)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY) + "px");
+        })
+        .on("mousemove", function(d) { 
+          div.style('top', (d3.event.pageY - 20) + "px")
+          .style('left', (d3.event.pageX + 20) + "px");
+        })
+        .on("mouseout", function (d) {
+          div.transition()
+            .duration(500)
+            .style("opacity", 0);
+        });
+
+
+
+      // Call the  axis
+        svgAgeStack.select('.x.axis')
+              .transition(t)
+              .call(x_axis);
 
         
-      
-      newBars.on("mouseover", function (d) {
-        div.transition()
-          .duration(200)
-          .style("opacity", .9);
+        svgAgeStack.select('.y.axis')
+              .transition(t)
+              .call(y_axis);
 
-        // subgroup Name and Value with reference to https://www.d3-graph-gallery.com/graph/barplot_stacked_hover.html 
-        let subgroupName = d3.select(this.parentNode).datum().key;
-        let subgroupValue = d.data[subgroupName];
-        div.html(`${subgroupName}: <br>${subgroupValue}`)
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY) + "px");
+
+        addLegendAgeStack(data)
+
       })
-      .on("mousemove", function(d) { 
-         div.style('top', (d3.event.pageY - 20) + "px")
-         .style('left', (d3.event.pageX + 20) + "px");
-      })
-      .on("mouseout", function (d) {
-        div.transition()
-          .duration(500)
-          .style("opacity", 0);
-      });
+    }
+    
+    showAgeStackChart();
+    d3.selectAll("input").on("change", function(){
+      console.log(this);
+      if (this.value == "stacked"){
+        showAgeStackChart();
+      } else {
+        showAgePercentStack();
+      }
+    })
+   
 
+ 
 
-
-     // Call the  axis
-      svgAgeStack.select('.x.axis')
-            .transition(t)
-            .call(x_axis);
-
-      svgAgeStack.select('.y.axis')
-            .transition(t)
-            .call(y_axis);
-
-
-      addLegendAgeStack(data)
-
-  });
-
-
-}
   function addLegendAgeStack(data){
 
   // add legend 
@@ -363,23 +353,12 @@ function percentStack(){
     });
   }
 
+  
   function type(d, i, columns) {
     for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
     d.total = t;
     return d;
   }
+  
 
  
-
-// showStackedChartTotal();
-
-// setTimeout(function(){
-//   console.log("new one")
-//   percentStack();
-// }, 3000);
-
-percentStack();
-setTimeout(function(){
-  console.log("new one")
-  showAgeStackChart();
-}, 3000);
